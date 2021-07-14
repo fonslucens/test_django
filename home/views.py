2748a0e87b2ff8f0a5156786ee38c09489fcb264
fonslucens <hyunii605@naver.com>
2021-07-14 10:44:53 +0900

    change views

--- home/views.py
+++ home/views.py
@@ -8,7 +8,7 @@ def index(request):
     if path == '/home':
         resultstr = '<h1>여기는 Home 입니다.</h1>'
     else :
-        resultstr = '<h1>여기는 Main 입니다.</h1>'
+        resultstr = '<h1>여기는 Master 입니다.</h1>'
 
     return HttpResponse(resultstr)
 
